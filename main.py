5cf7c5d96ebe249d571c49cb992789966c609542
yang543214 <35080386+yang543214@users.noreply.github.com>
2024-04-08 16:55:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -405,5 +405,5 @@ if __name__ == "__main__":
     # 公众号推送消息
     for user in users:
        send_message(user, accessToken, region, nextweather, nextmin_temp, nextmax_temp, weather, temp,
-                   wind_dir, love_days, note_ch, note_en, max_temp, min_temp, sunrise, sunset, category, pm2p5, proposal, chp)
+                   wind_dir, note_ch, note_en, max_temp, min_temp, sunrise, sunset, category, pm2p5, proposal, chp)
     os.system("pause")
